1629ec17945ab62b3723373b1d7006ecf680c926
Kareem Shehab <kshehab1@student.gsu.edu>
2022-10-06 17:37:26 -0700

    FRICK, fixing typo in migration...

--- src/migrations/1665102418396-ChangedDeleteNameAndAddEditCol.ts
+++ src/migrations/1665102418396-ChangedDeleteNameAndAddEditCol.ts
@@ -4,13 +4,13 @@ export class ChangedDeleteNameAndAddEditCol1665102418396 implements MigrationInt
     name = 'ChangedDeleteNameAndAddEditCol1665102418396'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "messages" RENAME COLUMN "deletedAt" to "deleted_at`);
+        await queryRunner.query(`ALTER TABLE "messages" RENAME COLUMN "deletedAt" to "deleted_at"`);
         await queryRunner.query(`ALTER TABLE "messages" ADD "edited_on" TIMESTAMP DEFAULT now()`);
         await queryRunner.query(`UPDATE "messages" SET "edited_on" = "posting_date"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "messages" RENAME COLUMN "deleted_at" to "deletedAt`);
+        await queryRunner.query(`ALTER TABLE "messages" RENAME COLUMN "deleted_at" to "deletedAt"`);
         await queryRunner.query(`ALTER TABLE "messages" DROP COLUMN "edited_on"`);
     }
 
